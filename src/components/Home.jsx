96ec26e9fee4ed4021408863ff1f2cd43b6e9920
Deshan Mclachlan <deshan.mclachlan0@gmail.com>
2023-11-23 10:33:41 +1300

    Added aditional information and classnames to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,10 +14,10 @@ const Home = () => {
           </ul>
         </div>
         <div className="contact-details">
-          <ul>
-            <li>Contact</li>
-            <li>+64 000 000 0000</li>
-            <li>tiarekelly@gmail.com</li>
+          <ul className="contact-details-list">
+            <li className="contact-details-list-item">Contact</li>
+            <li className="contact-details-list-item">+64 000 000 0000</li>
+            <li className="contact-details-list-item">tiarekelly@gmail.com</li>
           </ul>
         </div>
         <div className="works-page">
@@ -38,6 +38,9 @@ const Home = () => {
               <li className="work-headings-list-item">
                 <h3>TYPE</h3>
               </li>
+              <li className="work-headings-list-item">
+                <h3>DATE</h3>
+              </li>
             </ul>
           </div>
           <div className="selected-works">
@@ -46,36 +49,43 @@ const Home = () => {
                 <p>TEA PROMO VIDEO</p>
                 {/* Insert video content here*/}
                 <p>COMPOSITION & ARRANGEMENT</p>
+                <p>20.02.22</p>
               </li>
               <li className="selected-works-list-item">
                 <p>COMP</p>
                 {/* Insert video content here*/}
                 <p>COMPOSITION & ARRANGEMENT</p>
+                <p>15.11.22</p>
               </li>
               <li className="selected-works-list-item">
                 <p>ENSEMBLE</p>
                 {/* Insert video content here*/}
                 <p>COMPOSITION & ARRANGEMENT</p>
+                <p>06.04.21</p>
               </li>
               <li className="selected-works-list-item">
                 <p>SHE: A SPACE FOR WOMXN</p>
                 {/* Insert video content here*/}
                 <p>CURATION</p>
+                <p>04.10.21</p>
               </li>
               <li className="selected-works-list-item">
                 <p>BENEE TOUR</p>
                 {/* Insert video content here*/}
                 <p>LIVE PERFORMANCES & TOUR: LEAD GUITAR</p>
+                <p>20.01.18 - 17.09.23</p>
               </li>
               <li className="selected-works-list-item">
                 <p>BALLSY: SHORT FILM</p>
                 {/* Insert video content here*/}
                 <p>COMPOSITION & ARRANGEMENT</p>
+                <p>21.09.23</p>
               </li>
               <li className="selected-works-list-item">
                 <p>TEA PROMO VIDEO</p>
                 {/* Insert video content here*/}
                 <p>COMPOSITION & ARRANGEMENT</p>
+                <p>04.07.21</p>
               </li>
             </ul>
           </div>
