62b3b701d5bcdb5a14404b8180ffda9e7f07f62e
Deshan Mclachlan <deshan.mclachlan0@gmail.com>
2023-11-23 12:31:33 +1300

    Css edited in for nav and home.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,8 +3,8 @@ import "./Nav.css";
 const Nav = () => {
   return (
     <div className="sticky-nav">
-      <ul className="nav-list">
-        <li className="nav-list-item"></li>
+      <ul className="nav-list nav-grid">
+        <li className="nav-list-item">Home</li>
         <li className="nav-list-item">Work</li>
         <li className="nav-list-item">About</li>
         <li className="nav-list-item">Extras</li>
