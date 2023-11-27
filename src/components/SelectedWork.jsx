73d02c150958df0cd4ed7d8f0ad0c965805f8c25
Deshan Mclachlan <deshan.mclachlan0@gmail.com>
2023-11-27 20:14:03 +1300

    Continued css styling for selected works page

--- src/components/SelectedWork.jsx
+++ src/components/SelectedWork.jsx
@@ -1,4 +1,5 @@
 import "./SelectedWork.css";
+import horizon from "../assets/images/horizon.jpg";
 
 const SelectedWork = () => {
   return (
@@ -25,47 +26,57 @@ const SelectedWork = () => {
           </li>
         </ul>
         <ul className="selected-works-list">
-          <li className="selected-works-list-item">
-            <p>TEA PROMO VIDEO</p>
-            {/* Insert video content here*/}
-            <p>COMPOSITION & ARRANGEMENT</p>
-            <p>20.02.22</p>
+          <li className="selected-works-list-item selected-works-list-item-a">
+            <div className="selected-work-border-top" />
+            <p className="selected-work-heading">TEA PROMO VIDEO</p>
+            <img className="image selected-works-image" src={horizon} />
+            <p className="selected-work-type">COMPOSITION & ARRANGEMENT</p>
+            <p className="selected-work-date">20.02.22</p>
           </li>
-          <li className="selected-works-list-item">
-            <p>COMP</p>
-            {/* Insert video content here*/}
-            <p>COMPOSITION & ARRANGEMENT</p>
-            <p>15.11.22</p>
+          <li className="selected-works-list-item selected-works-list-item-b">
+            <div className="selected-work-border-top" />
+            <p className="selected-work-heading">COMP</p>
+            <img className="image selected-works-image" src={horizon} />
+            <p className="selected-work-type">COMPOSITION & ARRANGEMENT</p>
+            <p className="selected-work-date">15.11.22</p>
           </li>
-          <li className="selected-works-list-item">
-            <p>ENSEMBLE</p>
-            {/* Insert video content here*/}
-            <p>COMPOSITION & ARRANGEMENT</p>
-            <p>06.04.21</p>
+          <li className="selected-works-list-item selected-works-list-item-c">
+            <div className="selected-work-border-top" />
+            <p className="selected-work-heading">ENSEMBLE</p>
+            <img className="image selected-works-image" src={horizon} />
+            <p className="selected-work-type">COMPOSITION & ARRANGEMENT</p>
+            <p className="selected-work-date">06.04.21</p>
           </li>
-          <li className="selected-works-list-item">
-            <p>SHE: A SPACE FOR WOMXN</p>
-            {/* Insert video content here*/}
-            <p>CURATION</p>
-            <p>04.10.21</p>
+          <li className="selected-works-list-item selected-works-list-item-d">
+            <div className="selected-work-border-top" />
+            <p className="selected-work-heading">SHE: A SPACE FOR WOMXN</p>
+            <img className="image selected-works-image" src={horizon} />
+            <p className="selected-work-type">CURATION</p>
+            <p className="selected-work-date">04.10.21</p>
           </li>
-          <li className="selected-works-list-item">
-            <p>BENEE TOUR</p>
-            {/* Insert video content here*/}
-            <p>LIVE PERFORMANCES & TOUR: LEAD GUITAR</p>
-            <p>20.01.18 - 17.09.23</p>
+          <li className="selected-works-list-item selected-works-list-item-e">
+            <div className="selected-work-border-top" />
+            <p className="selected-work-heading">BENEE TOUR</p>
+            <img className="image selected-works-image" src={horizon} />
+            <p className="selected-work-type">
+              LIVE PERFORMANCES & TOUR: LEAD GUITAR
+            </p>
+            <p className="selected-work-date">20.01.18 - 17.09.23</p>
           </li>
-          <li className="selected-works-list-item">
-            <p>BALLSY: SHORT FILM</p>
-            {/* Insert video content here*/}
-            <p>COMPOSITION & ARRANGEMENT</p>
-            <p>21.09.23</p>
+          <li className="selected-works-list-item selected-works-list-item-f">
+            <div className="selected-work-border-top" />
+            <p className="selected-work-heading">BALLSY: SHORT FILM</p>
+            <img className="image selected-works-image" src={horizon} />
+            <p className="selected-work-type">COMPOSITION & ARRANGEMENT</p>
+            <p className="selected-work-date">21.09.23</p>
           </li>
-          <li className="selected-works-list-item">
-            <p>TEA PROMO VIDEO</p>
-            {/* Insert video content here*/}
-            <p>COMPOSITION & ARRANGEMENT</p>
-            <p>04.07.21</p>
+          <li className="selected-works-list-item selected-works-list-item-g">
+            <div className="selected-work-border-top" />
+            <p className="selected-work-heading">TEA PROMO VIDEO</p>
+            <img className="image selected-works-image" src={horizon} />
+            <p className="selected-work-type">COMPOSITION & ARRANGEMENT</p>
+            <p className="selected-work-date">04.07.21</p>
+            <div className="selected-work-border-bottom" />
           </li>
         </ul>
       </div>
