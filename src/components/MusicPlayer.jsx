416f6ca8daf2db40547915e7e51a157638e7dd58
Deshan Mclachlan <deshan.mclachlan0@gmail.com>
2023-11-28 20:11:01 +1300

    Music player implemented. Needs functionality for next and prev songs still

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -3,20 +3,63 @@ import { MdSkipNext } from "react-icons/md";
 import { MdSkipPrevious } from "react-icons/md";
 import { MdPlayArrow } from "react-icons/md";
 import { MdPause } from "react-icons/md";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+import song from "../assets/music/Mourn.mp3";
 
 const MusicPlayer = () => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
 
   const togglePlayPause = () => {
-    setIsPlaying(!isPlaying);
+    const prevValue = isPlaying;
+
+    setIsPlaying(!prevValue);
+    if (!prevValue) {
+      audioPlayer.current.play();
+      animationRef.current = requestAnimationFrame(whilePlaying);
+    } else {
+      audioPlayer.current.pause();
+      cancelAnimationFrame(animationRef.current);
+    }
   };
 
-  console.log(isPlaying);
+  const whilePlaying = () => {
+    progressBar.current.value = audioPlayer.current.currentTime;
+    changePlayerCurrentTime();
+    animationRef.current = requestAnimationFrame(whilePlaying);
+  };
+
+  const changeRange = () => {
+    audioPlayer.current.currentTime = progressBar.current.value;
+    changePlayerCurrentTime();
+  };
+
+  const changePlayerCurrentTime = () => {
+    setCurrentTime(progressBar.current.value);
+  };
+
+  const audioPlayer = useRef();
+  const progressBar = useRef();
+  const animationRef = useRef();
+
+  useEffect(() => {
+    const seconds = Math.floor(audioPlayer.current.duration);
+    setDuration(seconds);
+    progressBar.current.max = seconds;
+  }, [audioPlayer?.current?.loadedmetadata, audioPlayer?.current?.readyState]);
+
+  const calculateTime = (secs) => {
+    const minutes = Math.floor(secs / 60);
+    const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const seconds = Math.floor(secs % 60);
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${returnedMinutes} : ${returnedSeconds}`;
+  };
 
   return (
     <div>
-      <audio src="../assets/music/Mourn.mp3"></audio>
+      <audio ref={audioPlayer} src={song}></audio>
       <button>
         <MdSkipPrevious />
       </button>
@@ -27,11 +70,16 @@ const MusicPlayer = () => {
         <MdSkipNext />
       </button>
 
-      <div>0:00</div>
+      <div>{calculateTime(currentTime)}</div>
       <div>
-        <input type="range" />
+        <input
+          type="range"
+          defaultValue="0"
+          ref={progressBar}
+          onChange={changeRange}
+        />
       </div>
-      <div>3:00</div>
+      <div>{duration && !isNaN(duration) && calculateTime(duration)}</div>
     </div>
   );
 };
