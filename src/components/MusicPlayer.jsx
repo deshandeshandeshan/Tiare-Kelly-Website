cf449b246d9db34730adbdcae5eab9a6340cce3e
Deshan Mclachlan <deshan.mclachlan0@gmail.com>
2023-11-30 12:37:18 +1300

    configured a playlist of songs that the audio player plays, next and prev song buttons working

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -4,12 +4,13 @@ import { MdSkipPrevious } from "react-icons/md";
 import { MdPlayArrow } from "react-icons/md";
 import { MdPause } from "react-icons/md";
 import { useState, useRef, useEffect } from "react";
-import song from "../assets/music/Mourn.mp3";
+import playlist from "../data/Playlist";
 
 const MusicPlayer = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [duration, setDuration] = useState(0);
   const [currentTime, setCurrentTime] = useState(0);
+  const [currentSong, setCurrentSong] = useState(playlist[0]);
 
   const togglePlayPause = () => {
     const prevValue = isPlaying;
@@ -39,6 +40,31 @@ const MusicPlayer = () => {
     setCurrentTime(progressBar.current.value);
   };
 
+  const prevSong = () => {
+    {
+      const index = playlist.findIndex((x) => x.title == currentSong.title);
+      if (index == 0) {
+        setCurrentSong(playlist[playlist.length - 1]);
+      } else {
+        setCurrentSong(playlist[index - 1]);
+      }
+      audioPlayer.current.currentTime = 0;
+    }
+  };
+
+  const nextSong = () => {
+    const index = playlist.findIndex((x) => x.title == currentSong.title);
+
+    if (index == playlist.length - 1) {
+      setCurrentSong(playlist[0]);
+    } else {
+      setCurrentSong(playlist[index + 1]);
+    }
+    audioPlayer.current.currentTime = 0;
+  };
+
+  console.log(duration);
+
   const audioPlayer = useRef();
   const progressBar = useRef();
   const animationRef = useRef();
@@ -54,14 +80,19 @@ const MusicPlayer = () => {
     const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
     const seconds = Math.floor(secs % 60);
     const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
-    return `${returnedMinutes} : ${returnedSeconds}`;
+
+    if (isNaN(duration)) {
+      return "0:00";
+    } else {
+      return `${returnedMinutes} : ${returnedSeconds}`;
+    }
   };
 
   return (
     <div className="audio-player">
-      <audio ref={audioPlayer} src={song}></audio>
+      <audio ref={audioPlayer} autoPlay src={currentSong.url} />
       <div className="song-name">
-        <h3>Song Name</h3>
+        <h3>{currentSong.title}</h3>
       </div>
       <div className="audio-progress-bar">
         <input
@@ -74,11 +105,10 @@ const MusicPlayer = () => {
       </div>
       <div className="audio-controls">
         <small className="audio-time-before-after">
-          {calculateTime(currentTime)} /{" "}
-          {duration && !isNaN(duration) && calculateTime(duration)}
+          {calculateTime(currentTime)} /{calculateTime(duration)}
         </small>
         <div className="audio-control-buttons">
-          <button className="audio-button prev-song">
+          <button className="audio-button prev-song" onClick={prevSong}>
             <MdSkipPrevious />
           </button>
           <button
@@ -87,7 +117,7 @@ const MusicPlayer = () => {
           >
             {isPlaying ? <MdPause /> : <MdPlayArrow />}
           </button>
-          <button className="audio-button next-song">
+          <button className="audio-button next-song" onClick={nextSong}>
             <MdSkipNext />
           </button>
         </div>
---
+++ src/data/Playlist.js
@@ -0,0 +1,23 @@
+import mourn from "../assets/music/Mourn.mp3";
+import iceBoy from "../assets/music/ICE BOY.mp3";
+import givingUp from "../assets/music/Giving Up.mp3";
+
+const playlist = [
+  {
+    id: 1,
+    title: "Mourn - Corbin",
+    url: mourn,
+  },
+  {
+    id: 2,
+    title: "Ice Boy - Corbin",
+    url: iceBoy,
+  },
+  {
+    id: 3,
+    title: "Giving Up - Corbin",
+    url: givingUp,
+  },
+];
+
+export default playlist;
