06cdeb04cc792f2057b862ebd4315a4e2a43f9ea
Deshan Mclachlan <deshan.mclachlan0@gmail.com>
2024-02-07 13:10:26 +1300

    Taken the selected works in app and extrapolated it out to data

---
+++ src/assets/data/SelectedWorksData.jsx
@@ -0,0 +1,60 @@
+const data = [
+  {
+    Title: "TEA PROMO VIDEO",
+    SelectedWork: "hello",
+    Type: "COMPOSITION & ARRANGEMENT",
+    Description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    Date: "20.02.22",
+  },
+  {
+    Title: "COMP",
+    SelectedWork: "COMPOSITION & ARRANGEMENT",
+    Type: "COMPOSITION & ARRANGEMENT",
+    Description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    Date: "15.11.22",
+  },
+  {
+    Title: "ENSEMBLE",
+    SelectedWork: "hello",
+    Type: "COMPOSITION & ARRANGEMENT",
+    Description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    Date: "06.04.21",
+  },
+  {
+    Title: "SHE: A SPACE FOR WOMXN",
+    SelectedWork: "hello",
+    Type: "CURATION",
+    Description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    Date: "04.10.21",
+  },
+  {
+    Title: "BENEE TOUR",
+    SelectedWork: "hello",
+    Type: "LIVE PERFORMANCES & TOUR: LEAD GUITAR",
+    Description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    Date: "20.01.18 - 17.09.23",
+  },
+  {
+    Title: "BALLSY: SHORT FILM",
+    SelectedWork: "hello",
+    Type: "COMPOSITION & ARRANGEMENT",
+    Description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    Date: "21.09.23",
+  },
+  {
+    Title: "TEA PROMO VIDEO",
+    SelectedWork: "hello",
+    Type: "COMPOSITION & ARRANGEMENT",
+    Description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    Date: "04.07.21",
+  },
+];
+
+export default data;
