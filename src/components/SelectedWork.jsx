5b44b211797fbf8b596bdc50c51ec035aca46d23
Deshan Mclachlan <deshan.mclachlan0@gmail.com>
2024-04-18 16:31:44 +1000

    Made changes to the selectedworksdata page. Changed the information being fetched. Erased old css rules

--- src/components/SelectedWork.jsx
+++ src/components/SelectedWork.jsx
@@ -5,24 +5,7 @@ import SelectedWorksData from "./SelectedWorksData";
 const SelectedWork = () => {
   return (
     <div className="selected-works-page" id="selected-works">
-      <div className="selected-works-intro">
-        <h2 className="selected-works-intro-text">
-          Tiare Kelly is a Composer, Musician & Artist based in Tamaki Makaurau
-          (Auckland), these are her selected works.
-        </h2>
-      </div>
       <div className="selected-works">
-        <ul className="selected-works-headings-list">
-          <li className="headings-list-item headings-list-item-a">
-            <h3>TITLE</h3>
-          </li>
-          <li className="headings-list-item headings-list-item-b">
-            <h3>TYPE</h3>
-          </li>
-          <li className="headings-list-item headings-list-item-c">
-            <h3>DATE</h3>
-          </li>
-        </ul>
         <SelectedWorksData />
       </div>
     </div>
--- src/components/SelectedWorksData.jsx
+++ src/components/SelectedWorksData.jsx
@@ -4,14 +4,6 @@ import sanityClient from "../client";
 
 const SelectedWorksData = () => {
   const [selectedWorks, setSelectedWorks] = useState(null);
-  const [selected, setSelected] = useState(null);
-
-  const toggle = (i) => {
-    if (selected === i) {
-      return setSelected(null);
-    }
-    setSelected(i);
-  };
 
   useEffect(() => {
     const fetchData = () => {
@@ -19,10 +11,12 @@ const SelectedWorksData = () => {
         .fetch(
           `*[_type == "selectedWorks"]{
         title,
-        url,
         type,
-        description,
-        date
+        image {
+          asset -> {
+            url
+          }
+        }
       }`
         )
         .then((data) => setSelectedWorks(data))
@@ -30,49 +24,21 @@ const SelectedWorksData = () => {
     };
 
     fetchData();
+    console.log(selectedWorks);
   }, []);
-
-  console.log(selectedWorks);
 
   return (
     <ul>
       {selectedWorks &&
-        selectedWorks.map((selectedWork, i) => (
-          <li
-            key={selectedWork.id}
-            className={
-              selected === i
-                ? `selected-works-list-item open`
-                : `selected-works-list-item closed`
-            }
-            onClick={() => toggle(i)}
-          >
+        selectedWorks.map((selectedWork) => (
+          <li key={selectedWork.id} className="selected-works-list-item open">
             <div className="selected-work-border-top" />
             <p className="selected-work-heading">{selectedWork.title}</p>
             <p className="selected-work-type">{selectedWork.type}</p>
-            <p className="selected-work-date">{selectedWork.date}</p>
-            <p
-              className={
-                selected === i
-                  ? `selected-work-description show`
-                  : `selected-work-description hidden`
-              }
-            >
-              {selectedWork.description}
-            </p>
-            <div
-              className={
-                selected === i ? `video-wrapper open` : `video-wrapper closed`
-              }
-            >
-              <iframe
-                className="selected-works-video"
-                src={
-                  selected === i ? `${selectedWork.url}` : `${selectedWork.url}`
-                }
-                allow="autoplay; fullscreen; picture-in-picture"
-              />
-            </div>
+            <img
+              src={selectedWork.image.asset.url}
+              className="selected-work-date"
+            />
           </li>
         ))}
       <div className="selected-work-border-top" />
