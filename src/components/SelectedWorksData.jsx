9ac82ffe900c3fad311feca0fdcafc3ba629eadc
Deshan Mclachlan <deshan.mclachlan0@gmail.com>
2024-04-24 12:15:43 +1000

    reverted projects to be a mapped array so that I can create a flex animation rather than it being a carosell

--- src/components/SelectedWorksData.jsx
+++ src/components/SelectedWorksData.jsx
@@ -1,29 +1,14 @@
 import { useState, useEffect } from "react";
 import sanityClient from "../client";
 import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
-// import ReactPlayer from "react-player";
 
 const SelectedWorksData = () => {
   const [selectedWorks, setSelectedWorks] = useState(null);
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const goToPrev = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? selectedWorks.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-  };
-
-  const goToNext = () => {
-    const isLastSlide = currentIndex === selectedWorks.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-  };
 
   useEffect(() => {
-    async () => {
-      await sanityClient
-        .fetch(
-          `*[_type == "selectedWorks"]{
+    sanityClient
+      .fetch(
+        `*[_type == "selectedWorks"]{
         title,
         type,
         image {
@@ -32,31 +17,31 @@ const SelectedWorksData = () => {
           }
         }
       }`
-        )
-        .then((data) => setSelectedWorks(data))
-        .catch(console.error);
-    };
+      )
+      .then((data) => setSelectedWorks(data))
+      .catch(console.error);
   }, []);
 
   console.log(selectedWorks);
 
   return (
     <ul>
-      <li
-        key={selectedWorks[currentIndex].id}
-        className="selected-works-list-item open"
-      >
-        <p className="selected-work-heading">
-          {selectedWorks[currentIndex].title}
-        </p>
-        <p className="selected-work-type">{selectedWorks[currentIndex].type}</p>
-        <img
-          src={selectedWorks[currentIndex].image.asset.url}
-          className="selected-work-date"
-        />
-        <FaAngleLeft onClick={goToPrev} className="image-slider-left-arrow" />
-        <FaAngleRight onClick={goToNext} className="image-slider-right-arrow" />
-      </li>
+      {selectedWorks &&
+        selectedWorks.map((selectedWork, i) => (
+          <li
+            key={`selectedWork_${i}`}
+            className="selected-works-list-item open"
+          >
+            <p className="selected-work-heading">{selectedWork.title}</p>
+            <p className="selected-work-type">{selectedWork.type}</p>
+            <img
+              src={selectedWork.image.asset.url}
+              className="selected-work-date"
+            />
+            <FaAngleLeft className="image-slider-left-arrow" />
+            <FaAngleRight className="image-slider-right-arrow" />
+          </li>
+        ))}
     </ul>
   );
 };
