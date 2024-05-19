fa64ae8d1e99725e33b1fb549850b350ffa40b38
Deshan Mclachlan <deshan.mclachlan0@gmail.com>
2024-05-19 14:26:45 +1000

    selectedwork component now fetches data and finds matching object to id. displays correct project data

--- src/components/SelectedWork.jsx
+++ src/components/SelectedWork.jsx
@@ -1,13 +1,50 @@
 import { Link } from "react-router-dom";
 import "./SelectedWork.css";
+import sanityClient from "../client";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
 
 const SelectedWork = () => {
+  const [selectedWork, setSelectedWork] = useState([]);
+
+  const { id } = useParams();
+
+  const fetch = async (id) => {
+    const response = await sanityClient.fetch(
+      `*[_type == "selectedWorks"]{
+            title,
+            type,
+            description,
+            video,
+            role,
+            image {
+              asset -> {
+                url
+              }
+            }
+          }`
+    );
+
+    const filteredProject = response.find((work) => work.title === id);
+    const result = setSelectedWork(filteredProject);
+    return result;
+  };
+
+  useEffect(() => {
+    fetch(id);
+  }, [id]);
+
+  console.log(selectedWork);
+  console.log(id);
+
   return (
     <main className="selected-work-page">
       <div className="selected-work-grid">
-        <h2 className="selected-work-title">Ballsy: Film</h2>
+        <h2 className="selected-work-title">
+          {selectedWork.title}: {selectedWork.type}
+        </h2>
         <h2 className="selected-work-role selected-work-padding-bottom">
-          Composition, Lead guitar
+          {selectedWork.role}
         </h2>
         <Link to="/" className="selected-work-link">
           <p className="selected-work-link-to-home selected-work-padding-bottom">
@@ -18,18 +55,13 @@ const SelectedWork = () => {
       <div className="video-container">
         <iframe
           className="selected-work-video"
-          src="https://player.vimeo.com/video/939135094?h=0f78de8fa9"
+          src={selectedWork.video}
           allow="autoplay; fullscreen; picture-in-picture"
-          allowfullscreen
+          allowFullScreen
         ></iframe>
       </div>
       <p className="selected-work-description selected-work-padding-top">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut. Lorem ipsum
-        dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut.
+        {selectedWork.description}
       </p>
     </main>
   );
